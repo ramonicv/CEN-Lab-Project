eeb0b9704fe599f8f2cb5277e25797ecd48628dc
ramonicv <pepsidog0@proton.me>
2026-03-31 13:10:13 -0400

    added documentation to GameSession model

--- src/game/models.py
+++ src/game/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class GameSession(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    player_data = models.TextField(help_text="Stores the player names as a JSON string")
+    player_data = models.TextField(help_text="Stores the player names as a JSON string") # for debug purposes, can be changed to a more structured format later
 
     def __str__(self):
         return f"Session at {self.created_at}"
