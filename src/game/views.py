7c380bd4ff4da1538f41f59475485dd598098082
ramonicv <pepsidog0@proton.me>
2026-04-01 10:47:56 -0400

    refactored views.py removing POST logic and reordering functions to follow conventions

--- src/game/views.py
+++ src/game/views.py
@@ -1,18 +1,11 @@
-
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import GameSession
 import json
 
-def signup(request):
-    if request.method == 'POST':
-        # Grab the JSON string sent from the hidden form field
-        raw_data = request.POST.get('player_json_input')
-        if raw_data:
-            GameSession.objects.create(player_data=raw_data)
-            return redirect('signup')
-
-    return render(request, 'game/signup.html')
-
 def home(request):
     return render(request, 'game/home.html')
+
+def signup(request):
+    return render(request, 'game/signup.html')
+
