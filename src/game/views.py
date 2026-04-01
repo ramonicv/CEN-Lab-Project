21788940c291ee8c8e83828266174ecf26006be9
ramonicv <pepsidog0@proton.me>
2026-04-01 11:09:24 -0400

    removed unused import

--- src/game/views.py
+++ src/game/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import GameSession
-import json
 
 def home(request):
     return render(request, 'game/home.html')
